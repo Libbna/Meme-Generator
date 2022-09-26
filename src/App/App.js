3008fbf06e0ba6c04e1a54819881a1677eb24a04
Libbna Mathew <libbna.mathew@gmail.com>
2022-09-26 20:59:57 +0530

    created a structure and rendered a meme image

---
+++ src/App/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Meme from '../Components/Meme'
+
+const App = () => {
+  return (
+    <Meme />
+  )
+}
+
+export default App
---
+++ src/Components/Meme.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+
+const Meme = () => {
+
+    const [memes, setMemes] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.imgflip.com/get_memes').then(res =>
+            res.json().then(res => {
+                // console.log(res);
+
+                const memes = res.data.memes;
+                setMemes(memes);
+            }));
+    }, [])
+
+    return (
+        memes.length ? <img src={memes[0].url} /> : <></>
+    );
+};
+
+export default Meme;
